6a4a208c656156c59f604435e6f296db633659bd
Rishi Vora <rishivora@Rishis-MacBook-Air.local>
2022-07-07 14:31:51 -0700

    completion

--- assignment1.R
+++ assignment1.R
@@ -1,23 +1,20 @@
 # R Assignment 1
 
+x <- 9 # Create a variable 'x' that holds a value of 9
 
-# Create a variable 'x' that holds a value of 9
+y <- x # Create a variable 'y' that holds a value of x
 
-# Create a variable 'y' that holds a value of x
+z<- x + y # Create a variable 'z' that holds the sum of x and y
 
-# Create a variable 'z' that holds the sum of x and y
+rm(x) # Clear the variable x from the workspace
 
-# Clear the variable x from the workspace
+rm(list = ls()) # Clear all variables from the workspace
 
-# Clear all variables from the workspace
+browseURL("http://www.google.com") # Make a statement that directs to http://www.google.com
 
-# Make a statement that directs to http://www.google.com
+library() # Show a list of all packages installed
 
-# Show a list of all packages installed
-
-# Show a list of packages currently installed and usable
-
-
+search() # Show a list of packages currently installed and usable
 
 # Magic Number Exercise
 # Do the Following:
@@ -26,3 +23,12 @@
     # Subtract 6 from the answer.
     # Divide what you get by 3.
 # you should end up with the same number that you started with
+azero <- 10
+aone <- 10+2
+atwo <- aone*3
+athree <- atwo-6
+afour <- athree/3
+
+a <- 10
+a <- ((a+2)*3-6)/3
+
